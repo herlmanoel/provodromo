8b04f051d2d5d92c102f797e791ba1050a600fd5
herlmanoel <herlmanoelfernandes@gmail.com>
2024-03-26 18:45:52 -0300

    feat: add setup

---
+++ src/main/java/com/provodromo/provodromo/ProvodromoApplication.java
@@ -0,0 +1,13 @@
+package com.provodromo.provodromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvodromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvodromoApplication.class, args);
+	}
+
+}
