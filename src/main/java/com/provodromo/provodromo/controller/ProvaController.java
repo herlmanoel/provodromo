4775b9f9345a34caa19cae07cefa39345c8d1021
alicemlima <alicemariaferreiradelima@gmail.com>
2024-04-07 17:32:27 -0300

    Criação dos endpoints relativo a prova

--- src/main/java/com/provodromo/provodromo/controller/ProvaController.java
+++ src/main/java/com/provodromo/provodromo/controller/ProvaController.java
@@ -1,11 +1,62 @@
 package com.provodromo.provodromo.controller;
 
+import com.provodromo.provodromo.model.Prova;
+import com.provodromo.provodromo.service.ProvaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/prova")
 public class ProvaController {
 
+    @Autowired
+    private ProvaService provaService;
+
+    @GetMapping("/listarProvas")
+    public String listarProvas(Model model) {
+        model.addAttribute("provas" , provaService.findAll());
+        return "prova/listar";
+    }
+
+    //TODO filtrar procas
+    @GetMapping("/listarProvasAtivas")
+    public String listarProvasAtivas (Model model) {
+        model.addAttribute("provasAtivas", provaService.findAll());
+        return  "prova/listar";
+    }
+
+    @GetMapping("/formularioProva")
+    public String novaProvaFormulario(Model model) {
+        model.addAttribute("prova", new Prova());
+        return "prova/formulario";
+    }
+
+    @PostMapping("salvarProva")
+    public String salvarProva(@ModelAttribute Prova prova) {
+        provaService.save(prova);
+        return "questao/formulario";
+    }
+
+    @GetMapping("/formularioEdicao/{id}")
+    public String editarProvaFormulario(@PathVariable Long id, Model model) {
+        Prova prova = provaService.findById(id);
+        model.addAttribute("prova", prova);
+        return "prova/formulario";
+    }
+
+    @PostMapping("/editar/{id}")
+    public String editarProva(@PathVariable Long id, Prova prova) {
+        prova.setId(id);
+        provaService.save(prova);
+        return "redirect:/prova/listar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirProva(@PathVariable Long id, Model model) {
+        provaService.deleteById(id);
+        return "redirect:/prova/listar";
+    }
 
 }
