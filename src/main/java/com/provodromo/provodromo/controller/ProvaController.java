aae82ea3c6bdd9738e6715a95bb0a7841f9b44ef
alicemlima <alicemariaferreiradelima@gmail.com>
2024-04-07 19:40:00 -0300

    Criação de telas para prova

--- src/main/java/com/provodromo/provodromo/controller/ProvaController.java
+++ src/main/java/com/provodromo/provodromo/controller/ProvaController.java
@@ -2,6 +2,7 @@ package com.provodromo.provodromo.controller;
 
 import com.provodromo.provodromo.model.Prova;
 import com.provodromo.provodromo.service.ProvaService;
+import com.provodromo.provodromo.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,49 +15,53 @@ public class ProvaController {
     @Autowired
     private ProvaService provaService;
 
+    @Autowired
+    private TurmaService turmaService;
     @GetMapping("/listarProvas")
     public String listarProvas(Model model) {
         model.addAttribute("provas" , provaService.findAll());
-        return "prova/listar";
+        return "prova/listarProvas";
     }
 
     //TODO filtrar procas
     @GetMapping("/listarProvasAtivas")
     public String listarProvasAtivas (Model model) {
         model.addAttribute("provasAtivas", provaService.findAll());
-        return  "prova/listar";
+        return  "prova/listarProvas";
     }
 
     @GetMapping("/formularioProva")
     public String novaProvaFormulario(Model model) {
         model.addAttribute("prova", new Prova());
-        return "prova/formulario";
+        model.addAttribute("turmas", turmaService.findAll());
+
+        return "prova/formularioProva";
     }
 
     @PostMapping("salvarProva")
     public String salvarProva(@ModelAttribute Prova prova) {
         provaService.save(prova);
-        return "questao/formulario";
+        return "prova/listarProvas";
     }
 
     @GetMapping("/formularioEdicao/{id}")
     public String editarProvaFormulario(@PathVariable Long id, Model model) {
         Prova prova = provaService.findById(id);
         model.addAttribute("prova", prova);
-        return "prova/formulario";
+        return "prova/formularioProva";
     }
 
     @PostMapping("/editar/{id}")
     public String editarProva(@PathVariable Long id, Prova prova) {
         prova.setId(id);
         provaService.save(prova);
-        return "redirect:/prova/listar";
+        return "redirect:/prova/listarProva";
     }
 
     @GetMapping("/excluir/{id}")
     public String excluirProva(@PathVariable Long id, Model model) {
         provaService.deleteById(id);
-        return "redirect:/prova/listar";
+        return "redirect:/prova/listarProva";
     }
 
 }
--- src/main/java/com/provodromo/provodromo/model/Turma.java
+++ src/main/java/com/provodromo/provodromo/model/Turma.java
@@ -2,17 +2,18 @@ package com.provodromo.provodromo.model;
 
 import com.provodromo.provodromo.model.base.BaseModel;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
-
-import java.util.List;
 
 @Entity
 @Table(name = "tb_turma")
 @Data
 public class Turma extends BaseModel {
-
     private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
     private Usuario professor;
-    private List<Usuario> alunos;
 }
