54c8921ddea5dfdfa283a102650cff8a271ffe57
alicemlima <alicemariaferreiradelima@gmail.com>
2024-04-09 20:30:22 -0300

    alteracoes em tipo usuario

--- src/main/java/com/provodromo/provodromo/model/TipoUsuario.java
+++ src/main/java/com/provodromo/provodromo/model/TipoUsuario.java
@@ -4,10 +4,14 @@ import com.provodromo.provodromo.model.base.BaseModel;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_tipo_usuario")
 @Data
 public class TipoUsuario extends BaseModel {
-
     private String nome;
+
+//    @OneToMany(mappedBy = "tipoUsuario", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+//    private List<Usuario> usuarios;
 }
